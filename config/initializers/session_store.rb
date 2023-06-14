3e9c627c0cf596d8e6054675fcf80fe48ccb31f0
Kelvin-Ben <kevoben14@gmail.com>
2023-06-14 18:40:17 +0300

    Provide a production environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "dh-authentication-app-react.heroku.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "jdh-authentication-app-react.heroku.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "authentication_app"
+end
